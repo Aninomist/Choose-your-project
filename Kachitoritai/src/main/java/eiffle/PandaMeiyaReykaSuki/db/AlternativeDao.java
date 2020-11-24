0d879bc36ba749172e1e90d09f2fd49ff28b4c8f
Aninomist <54560028+Aninomist@users.noreply.github.com>
2020-11-24 00:59:44 -0500

    Update AlternativeDao.java

--- Kachitoritai/src/main/java/eiffle/PandaMeiyaReykaSuki/db/AlternativeDao.java
+++ Kachitoritai/src/main/java/eiffle/PandaMeiyaReykaSuki/db/AlternativeDao.java
@@ -1,7 +1,11 @@
 package eiffle.PandaMeiyaReykaSuki.db;
 
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+
+import eiffle.PandaMeiyaReykaSuki.model.Alternative;
 
 public class AlternativeDao {
 
@@ -34,17 +38,11 @@ public class AlternativeDao {
 		}
 	}
 	
-	public List<> createAlternatives(int numAlt, String choiceID) throws Exception {
+	public List<Alternative> getAlternatives(int numAlt, String choiceID) throws Exception {
+		List<Alternative> allAlternatives = new ArrayList<>();
 		try {
-			for (int i = 0; i < numAlt; i++) {
-				String altID = UUID.randomUUID().toString();
-				PreparedStatement ps = conn.prepareStatement("INSERT INTO " + tblName +
-						" (altID, choiceID, isFinalAlternative) values (?,?,FALSE)");
-				ps.setString(1, altID);
-				ps.setString(2, choiceID);
-				ps.execute();
-			}
-			return true;
+	
+	
 			
 		} catch(Exception e) {
 			throw new Exception("Falied to create Alternatives: " + e.getMessage());
