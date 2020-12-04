cdc0464f768cc8f57ff8fc7a4b6a55f7c02ca307
Aninomist <54560028+Aninomist@users.noreply.github.com>
2020-12-03 20:24:42 -0500

    Updated GetAlternativeRequest

--- Kachitoritai/src/main/java/eiffle/PandaMeiyaReykaSuki/http/GetAlternativeRequest.java
+++ Kachitoritai/src/main/java/eiffle/PandaMeiyaReykaSuki/http/GetAlternativeRequest.java
@@ -2,17 +2,15 @@ package eiffle.PandaMeiyaReykaSuki.http;
 
 public class GetAlternativeRequest {
 	public String choiceID;
-	public int noAlter;
 	
 	public String getChoiceID() { return choiceID; }
 	public void setChoiceID(String id) { this.choiceID=id; }
 	
-	public int getNoAlter() { return noAlter; }
-	public void setNoAlter(int noAlter) { this.noAlter=noAlter; }
+//	public int getNoAlter() { return noAlter; }
+//	public void setNoAlter(int noAlter) { this.noAlter=noAlter; }
 	
-	public GetAlternativeRequest(String choiceID, int noAlter) {
+	public GetAlternativeRequest(String choiceID) {
 		this.choiceID = choiceID;
-		this.noAlter = noAlter;
 	}
 	
 	public GetAlternativeRequest() {}
