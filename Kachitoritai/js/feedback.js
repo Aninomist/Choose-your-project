9015702e603402606c9b13e1b702a3fbe09f4bbc
SukiyAkiOhno <73395799+SukiyAkiOhno@users.noreply.github.com>
2020-12-08 08:21:32 +0800

    comment out the duplicate part on Alt html and update the feedback js

--- Kachitoritai/js/feedback.js
+++ Kachitoritai/js/feedback.js
@@ -11,4 +11,31 @@ function handleFeedbackClick(username, numAlt, feedback) {
 
     // send the collected data as JSON
     xhr.send(js);
+
+    // This will process results and update HTML as appropriate.
+   
+
+    xhr.onloadend = function () {
+        console.log(xhr);
+        console.log(xhr.request);
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+
+            var responseJs = JSON.parse(xhr.responseText);
+            //choiceID = responseJs.response;
+            reponseCode = responseJs.httpCode;
+
+            if (reponseCode == 200) {
+                getAlternative(document.choiceID.value);
+            } else if (responseCode == 405) {
+                alert("Choice already Concluded, can't vote anymore");
+                //hide buttons
+            }
+            else {
+                alert("unable to process request");
+            }
+        } else {
+            //processResponse("N/A");
+        }
+    };
+               
 }
