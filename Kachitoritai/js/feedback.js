989cf9fd7ea48589e9ac05bf3b9491503d9c96fa
SukiyAkiOhno <73395799+SukiyAkiOhno@users.noreply.github.com>
2020-12-09 02:51:28 +0800

    edited feedback.js

--- Kachitoritai/js/feedback.js
+++ Kachitoritai/js/feedback.js
@@ -1,31 +1,122 @@
 const createFeedBack_url = "https://bxden5df79.execute-api.us-east-2.amazonaws.com/delta/addFeedback";
-const getFeedback_url = "";
+
 
 function processFeedback(result) {
     console.log("result:" + result);
 
     var js = JSON.parse(result);
-    var feedbacks = js["feedbacks"];
-    var length = feedbacks.length;
+    var alternatives = js["alt"];
+    var length = alternative.length;
 
-    const textarea = document.getElementById('feedbackList1');
+    for (var j = 0; j < length; j++) {
+        if (alternatives[j]["noAlter"] == 1) {
+            var alt1 = alternatives[j];
+            feedBacks = alt1["feedBacks"];
+            const textarea = document.getElementById('feedbackList1');
 
-    if (length === 0) {
-        textarea.value = "No feedback";
-    } else {
-        let temp;
+            if (length === 0) {
+                textarea.value = "No feedback";
+            } else {
+                var temp;
 
-        for (let i = 0; i < length; i++) {
-            temp = "";
-            temp += "Feedback #" + i.toString() + ": ";
-            temp += "FeedbackID: " + feedbacks[i]["feedbackID"] + "; ";
-            temp += "Timestamp: " + feedbacks[i]["dateCreated"] + "; ";
-            temp += "Owner: " + feedbacks[i]["username"] + "; ";
-            temp += "Alternative ID: " + feedbacks[i]["altID"] + "; ";
-            temp += "Description: " + feedbacks[i]["description"] + "\n\n";
-            textarea.value += temp;
+                for (let i = 0; i < feedBacks.length; i++) {
+                    temp = "";
+                    temp += "Feedback #" + i.toString() + ": ";
+                    temp += "FeedbackID: " + feedbacks[i]["feedbackID"] + "; ";
+                    temp += "Timestamp: " + feedbacks[i]["dateCreated"] + "; ";
+                    temp += "Owner: " + feedbacks[i]["username"] + "; ";
+                    temp += "Alternative ID: " + feedbacks[i]["altID"] + "; ";
+                    temp += "Description: " + feedbacks[i]["description"] + "\n\n";
+                    textarea.value += temp;
+                }
+            }
+        }
+        if (alternatives[j]["noAlter"] == 2) {
+            const textarea = document.getElementById('feedbackList2');
+            var alt2 = alternatives[j];
+            feedBacks = alt2["feedBacks"];
+            if (length === 0) {
+                textarea.value = "No feedback";
+            } else {
+                var temp;
+
+                for (var i = 0; i < feedBacks.length; i++) {
+                    temp = "";
+                    temp += "Feedback #" + i.toString() + ": ";
+                    temp += "FeedbackID: " + feedbacks[i]["feedbackID"] + "; ";
+                    temp += "Timestamp: " + feedbacks[i]["dateCreated"] + "; ";
+                    temp += "Owner: " + feedbacks[i]["username"] + "; ";
+                    temp += "Alternative ID: " + feedbacks[i]["altID"] + "; ";
+                    temp += "Description: " + feedbacks[i]["description"] + "\n\n";
+                    textarea.value += temp;
+                }
+            }
+        }
+        if (alternatives[j]["noAlter"] == 3) {
+            const textarea = document.getElementById('feedbackList3');
+            var alt3 = alternatives[j];
+            feedBacks = alt3["feedBacks"];
+            if (length === 0) {
+                textarea.value = "No feedback";
+            } else {
+                var temp;
+
+                for (var i = 0; i < feedBacks.length; i++) {
+                    temp = "";
+                    temp += "Feedback #" + i.toString() + ": ";
+                    temp += "FeedbackID: " + feedbacks[i]["feedbackID"] + "; ";
+                    temp += "Timestamp: " + feedbacks[i]["dateCreated"] + "; ";
+                    temp += "Owner: " + feedbacks[i]["username"] + "; ";
+                    temp += "Alternative ID: " + feedbacks[i]["altID"] + "; ";
+                    temp += "Description: " + feedbacks[i]["description"] + "\n\n";
+                    textarea.value += temp;
+                }
+            }
+        }
+        if (alternatives[j]["noAlter"] == 4) {
+            const textarea = document.getElementById('feedbackList4');
+            var alt4 = alternatives[j];
+            feedBacks = alt4["feedBacks"];
+            if (length === 0) {
+                textarea.value = "No feedback";
+            } else {
+                var temp;
+
+                for (var i = 0; i < feedBacks.length; i++) {
+                    temp = "";
+                    temp += "Feedback #" + i.toString() + ": ";
+                    temp += "FeedbackID: " + feedbacks[i]["feedbackID"] + "; ";
+                    temp += "Timestamp: " + feedbacks[i]["dateCreated"] + "; ";
+                    temp += "Owner: " + feedbacks[i]["username"] + "; ";
+                    temp += "Alternative ID: " + feedbacks[i]["altID"] + "; ";
+                    temp += "Description: " + feedbacks[i]["description"] + "\n\n";
+                    textarea.value += temp;
+                }
+            }
+        }
+        if (alternatives[j]["noAlter"] == 5) {
+            const textarea = document.getElementById('feedbackList5');
+            var alt5 = alternatives[j];
+            feedBacks = alt5["feedBacks"];
+            if (length === 0) {
+                textarea.value = "No feedback";
+            } else {
+                var temp;
+
+                for (var i = 0; i < feedBacks.length; i++) {
+                    temp = "";
+                    temp += "Feedback #" + i.toString() + ": ";
+                    temp += "FeedbackID: " + feedbacks[i]["feedbackID"] + "; ";
+                    temp += "Timestamp: " + feedbacks[i]["dateCreated"] + "; ";
+                    temp += "Owner: " + feedbacks[i]["username"] + "; ";
+                    temp += "Alternative ID: " + feedbacks[i]["altID"] + "; ";
+                    temp += "Description: " + feedbacks[i]["description"] + "\n\n";
+                    textarea.value += temp;
+                }
+            }
         }
     }
+   
 
 }
 
@@ -52,12 +143,12 @@ function handleFeedbackClick(username, numAlt, feedback) {
         console.log(xhr.request);
         if (xhr.readyState == XMLHttpRequest.DONE) {
 
-            let responseJs = JSON.parse(xhr.responseText);
-            let reponseCode = responseJs.httpCode;
+            var responseJs = JSON.parse(xhr.responseText);
+            var reponseCode = responseJs.httpCode;
 
             if (reponseCode === 200) {
 
-                xhr.open("POST", getFeedback_url, true);
+                xhr.open("POST", getAlternative_url, true);
 
                 var data = {};
                 var js = JSON.stringify(data);
